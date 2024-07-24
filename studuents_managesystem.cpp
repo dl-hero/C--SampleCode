b321cc0c7a7c7c0293cf192f780f495ade616333
dl-hero <dl_hero@hotmail.com>
2024-07-24 23:25:58 +0800

    add student manage system

---
+++ studuents_managesystem.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+//学科科目类
+class Subject
+{
+	
+}
+
+
+
+
+int main()
+{
+	StudentList stulist;
+	stulist.init();	//读入文件数据初始化
+	stulist.menu();	//打开主菜单
+
+	return 0;
+}
