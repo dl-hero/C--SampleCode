5acd945c87c6bf2b3f3da84f1b142c3c442f10df
dl-hero <dl_hero@hotmail.com>
2024-09-20 21:48:38 +0800

    update function for snake

--- snake.cpp
+++ snake.cpp
@@ -7,22 +7,22 @@
 
 using namespace std;
 
-//定义方向
+//定义方向 限定作用域的枚举类，Direction与UP/DOWN/LEFT/RIGHT作用域相同，相当于等价定义 UP=0,DOWN=1,LEFT=2,RIGHT=3
 enum class Direction {UP,DOWN,LEFT,RIGHT};
 
 class SnakeGame
 {
     public:
-        //构造函数，初始化
+        //构造函数，初始化 width和height是通过参数初始化，score和gameover默认值初始化
         SnakeGame(int width,int height):width(width),height(height),score(0),gameover(false)
         {
-            srand(time(nullptr));
+            srand(time(nullptr));   //设置随机种子，nullptr:即空指针（null point）也可直接改为NULL或0
             initscr();  //初始化屏幕
-            keypad(stdscr,true);    //启用键盘输入
-            noecho();   //关闭回显
-            nodelay(stdscr,true);   //非阻塞模式
+            keypad(stdscr,true);    //启用/激活键盘输入,允许getch()函数获取功能键
+            noecho();   //关闭回显,或者叫禁止输入字符显示到现在屏幕上
+            nodelay(stdscr,true);   //非阻塞模式,多线程并发输入，使程序不用一直在getch()等待用户输入
             curs_set(0);    //隐藏光标
-            getmaxyx(stdscr,max_y,max_x);
+            getmaxyx(stdscr,max_y,max_x);   //获取窗口大小的函数,当前屏幕的行数(max_y)和列数(max_x)
             window=newwin(height,width,(max_y-height)/2,(max_x-width)/2);
             box(window,0,0);    //绘制窗口边框
             refresh();
@@ -47,7 +47,7 @@ class SnakeGame
                 handleInput(ch);
                 moveSnake();
                 checkCollision();
-                std::this_thread::sleep_for(std::chrono::milliseconds(900));
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
            }
            showGameOver();
         }
@@ -71,7 +71,22 @@ class SnakeGame
                 food.second=rand()%(width-2)+1;
             }
             while(isSnakeCell(food.first,food.second));
-            mvwaddch(window,food.first,food.second,'@');
+//测试是否支持颜色显示，可以支持
+/*            if(false==has_colors())
+            {
+                cout<<"not support color!!!"<<endl;
+            }
+            else
+            {
+                cout<<"support color!!!"<<endl;
+            }
+*/
+            start_color();  //开启颜色功能
+            init_pair(1,COLOR_RED,COLOR_BLACK);     //初始化颜色对，红字黑背
+            attron(COLOR_PAIR(1));  //打印不同颜色字体
+//            mvwaddch(window,food.first,food.second,'@');    //打印食物的位置
+            printw("test color!!!");    //打印食物的位置
+            attroff(COLOR_PAIR(1)); //打印完成颜色字体
             refresh();
             wrefresh(window); 
        }
@@ -183,9 +198,10 @@ class SnakeGame
        {
             clear();        //清屏幕
             string gameOverText="Game Over!";
-//            mvprintw(max_y/2,(max_x-gameOverText.length())/2,gameOverText.c_str());     //在指定位置打印,c_srt()返回字符串首地址
+            mvprintw(max_y/2,(max_x-gameOverText.length())/2,"%s",gameOverText.c_str());     //在指定位置打印,c_srt()返回字符串首地址
+//            mvprintw(max_y/2,(max_x-gameOverText.length())/2,"Game Over!");     //在指定位置打印,c_srt()返回字符串首地址
             string scoreText="Score: "+to_string(score);    //to_string(xxx)将括号内数字转换称字符串
-//            mvprintw(max_y/2+1,(max_x-scoreText.length())/2,scoreText.c_str());
+            mvprintw(max_y/2+1,(max_x-scoreText.length())/2,"%s",scoreText.c_str());
             refresh();  //将stdscr缓冲区中的数据显示在屏幕上
             getch();    //读取一个字符
 
