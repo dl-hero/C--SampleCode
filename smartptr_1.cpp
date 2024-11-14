2f2867517af85d5275f48f6bcfd397a472e4f615
dl-hero <dl_hero@hotmail.com>
2024-11-14 22:07:37 +0800

    modfied

--- smartptr_1.cpp
+++ smartptr_1.cpp
@@ -8,7 +8,7 @@ void Check(weak_ptr<int>& wp)
 {
     shared_ptr<int> sp=wp.lock();   //获取weak_ptr指针指向的shared_ptr的实例
 
-    if(nullprt!=sp)
+    if(nullptr!=sp)
         cout<<"still"<<endl;
     else
         cout<<"pointer is invaild"<<endl;
@@ -20,8 +20,24 @@ int main()
    shared_ptr<int> sp2=sp1;
    weak_ptr<int> wp=sp1;    //weak_ptr定义的wp指向shared_ptr<int>定义的sp1实例
 
-   cout<<*sp1<<endl;
-   cout<<*sp2<<endl;
+   cout<<*sp1<<endl;    //40
+   cout<<*sp2<<endl;    //40
+    cout<<sp1.use_count()<<endl;    //use_count()：返回智能指针所指向变量的被引用次数 //2
+    cout<<sp2.use_count()<<endl;    //2
+    Check(wp);  //still
+    sp1.reset();    //重置智能指针，使它所支持的资源为空
+    cout<<sp1<<endl;    //000000000
+//    cout<<*sp1<<endl;   //0
 
+    cout<<*sp2<<endl;       //40
+    cout<<sp1.use_count()<<endl;    //0
+    cout<<sp2.use_count()<<endl;    //2
+    cout<<sp2.unique()<<endl;       //1
+    Check(wp);  //still
+    sp2.reset();
+    cout<<sp2.use_count()<<endl;    //0
+    Check(wp);      //pointer is invalid
+
+    return 0;
 
 }
